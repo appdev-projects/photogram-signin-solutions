2ee047771d0c722b7f6ef193739d00cc654cc865
Raghu Betina <raghu@firstdraft.com>
2019-05-28 07:21:25 -0500

    Update users to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   match("/", { :controller => "pictures", :action => "recent", :via => "get" })
 
   match("/recent", { :controller => "pictures", :action => "recent", :via => "get" })
